118c1d17a9dcfdd222030a21842e47d26edd0205
James Stapleton <github@tasermonkeys.com>
2020-01-26 15:44:04 -0500

    Text area list auto-updating its height properly (including word wrap, and initial height)

--- my-app/src/components/textarea-list.tsx
+++ my-app/src/components/textarea-list.tsx
@@ -1,7 +1,8 @@
-import React, {useState, SyntheticEvent} from 'react';
+import React, {useState, SyntheticEvent, useEffect, createRef} from 'react';
 import classNames from 'classnames';
 import {useDebouncedCallback} from "use-debounce";
 import getLineHeight from 'line-height';
+import './textarea-list.css'
 
 interface TextareaDescriptor {
     lineHeight: number;
@@ -13,16 +14,28 @@ interface TextareaListProps {
     onCommit?: (committedItems: string[]) => void;
 }
 
-function showIfNonEmpty(container: string|any[]) {
-    return { 'show': (container.length > 0) };
+function showIfNonEmpty(container: string | any[]) {
+    return {'show': (container && container.length > 0)};
 }
 
 export function TextareaList(props: TextareaListProps) {
-    const presetValueString = (props.presetValues || []).join('\n');
-    const [pendingValueString, setPendingValueString] = useState(presetValueString);
+    // I can't figure out another way to get a handle on the TextArea at 'init' after the component has mounted,
+    // to auto-set the initial height of the TextArea
+    const taRef = createRef<HTMLTextAreaElement>();
+    const [pendingValueString, setPendingValueString] = useState<string>('');
     const [textareaDescriptor, setTextareaDescriptor] = useState<TextareaDescriptor>();
 
-    const [debouncedResizer] = useDebouncedCallback(resizeTextArea, 200, { maxWait: 400 });
+    const [debouncedResizer] = useDebouncedCallback(resizeTextArea, 200, {maxWait: 400});
+
+    // I was kind of hoping this would cause an 'onChange' to happen, but it doesn't
+    useEffect(() => {
+        // anytime something outside changes our 'presetValue' update the current textarea value and then
+        // resize.
+        setPendingValueString((props.presetValues || []).join('\n'));
+        if (taRef.current !== null) {
+            debouncedResizer(taRef.current);
+        }
+    }, [props.presetValues]);
 
     function debouncedEventResizer(e: SyntheticEvent<HTMLTextAreaElement>) {
         debouncedResizer(e.currentTarget);
@@ -41,7 +54,9 @@ export function TextareaList(props: TextareaListProps) {
     function resizeTextArea(ta: HTMLTextAreaElement) {
         const descriptor = fetchTextareaDescriptor(ta);
         ta.style.maxHeight = (props.maxRows && `${props.maxRows * descriptor.lineHeight}px`) || 'initial';
-        ta.style.height = `${pendingValueString.split('\n').length * descriptor.lineHeight}px`;
+        //ta.style.height = `${pendingValueString && pendingValueString.split('\n').length * descriptor.lineHeight}px`;
+        ta.style.height = '0';
+        ta.style.height = `${ta.scrollHeight}px`;
     }
 
     function commitHandler() {
@@ -55,15 +70,21 @@ export function TextareaList(props: TextareaListProps) {
 
     return <div className="textarea-list">
         <textarea
+            ref={taRef}
+            className="textarea-list-text"
             value={pendingValueString}
             onKeyDown={debouncedEventResizer}
-            onChange={e => { setPendingValueString(e.target.value); debouncedEventResizer(e); }}
+            onChange={e => {
+                setPendingValueString(e.currentTarget.value);
+                debouncedEventResizer(e);
+            }}
         />
         <button className={classNames('commit-button', showIfNonEmpty(pendingValueString))}
-                onClick={commitHandler}>Add</button>
+                onClick={commitHandler}>Add
+        </button>
     </div>
 }
 
 export function calculateTextareaDescriptor(textArea: HTMLTextAreaElement): TextareaDescriptor {
-    return { lineHeight: getLineHeight(textArea) };
+    return {lineHeight: getLineHeight(textArea)};
 }
